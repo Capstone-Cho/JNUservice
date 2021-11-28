8875fd8471e2c9015abb014da61ff7dc970b3a64
C7C4FF <80257041+C7C4FF@users.noreply.github.com>
2021-11-28 17:48:09 +0900

    3차

--- MERN_taximate_shop/client/src/components/views/DetailVideoPage/Sections/SideVideo.js
+++ MERN_taximate_shop/client/src/components/views/DetailVideoPage/Sections/SideVideo.js
@@ -1,55 +1,65 @@
-import React, {useEffect, useState} from 'react'
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import moment from "moment";
+
 function SideVideo() {
-    const [SideVideos, setSideVideos] = useState([])
+  const [SideVideos, setSideVideos] = useState([]);
 
-    useEffect(() => {
-        axios.get('/api/video/getVideos').then(response => {
-            if (response.data.success) {
-                // console.log(response.data.videos)
-                setSideVideos(response.data.videos)
-            } else {
-                alert('Failed to get Videos')
-            }
-        })
-    }, [])
+  useEffect(() => {
+    axios.get("/api/video/getVideos").then((response) => {
+      if (response.data.success) {
+        // console.log(response.data.videos)
+        setSideVideos(response.data.videos);
+      } else {
+        alert("Failed to get Videos");
+      }
+    });
+  }, []);
 
-    const sideVideoItem = SideVideos.map((video, index) => {
-        var minutes = Math.floor(video.duration / 60)
-        var seconds = Math.floor(video.duration - minutes * 60)
-
-        return (
-            <div key={index} style={{display: 'flex', marginTop: '1rem', padding: '0 2rem'}}>
-                <div style={{width: '40%', marginRight: '1rem'}}>
-                    <a href={`/video/${video._id}`} style={{color: 'gray'}}>
-                        <img src={`https://img.icons8.com/fluency/2x/carpool.png`} style={{width: '30px', margin: '30px'}} />
-                    </a>
-                </div>
-
-                <div style={{width: '50%'}}>
-                    <a href={`/video/${video._id}`} style={{color: 'gray'}}>
-                        <span style={{fontSize: '1rem', color: 'black'}}>{video.title} </span>
-                        <br />
-                        <span>이름: {video.writer.name}</span>
-                        <br />
-                        <span>조회: {video.views}</span>
-                        <br />
-                        <span>
-                            {minutes} : {seconds}
-                        </span>
-                        <br />
-                    </a>
-                </div>
-            </div>
-        )
-    })
-
+  const sideVideoItem = SideVideos.map((video, index) => {
     return (
-        <React.Fragment>
-            <div style={{marginTop: '3rem'}}></div>
-            {sideVideoItem}
-        </React.Fragment>
-    )
+      <div
+        key={index}
+        style={{ display: "flex", marginTop: "1rem", padding: "0 2rem" }}
+      >
+        <div style={{ width: "40%", marginRight: "1rem" }}>
+          <a href={`/video/${video._id}`} style={{ color: "gray" }}>
+            <img
+              src={`https://img.icons8.com/fluency/2x/carpool.png`}
+              style={{ width: "30px", margin: "30px" }}
+            />
+          </a>
+        </div>
+
+        <div style={{ width: "50%" }}>
+          <a href={`/video/${video._id}`} style={{ color: "gray" }}>
+            <span style={{ fontSize: "1rem", color: "black" }}>
+              {video.title}{" "}
+            </span>
+            <br />
+            <span>
+              {video.StartLocation} &#62; {video.EndLocation}
+            </span>
+            <br />
+            <span>
+              {moment(video.MeetTime).format("YYYY년 MM월 D일")}
+              <br></br>
+              {moment(video.MeetTime).format("hh:mm 예정")}
+            </span>
+            <br />
+            <br />
+          </a>
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <React.Fragment>
+      <div style={{ marginTop: "3rem" }}></div>
+      {sideVideoItem}
+    </React.Fragment>
+  );
 }
 
-export default SideVideo
+export default SideVideo;
